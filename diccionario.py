87d76245804c7c51ee7bf54a30d7a240e0a39295
javiersanchzgil <javsan675@alu.centroafuera.es>
2022-10-19 10:37:08 +0200

    Dadosymonedas acabado

--- diccionario.py
+++ diccionario.py
@@ -4,6 +4,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+#creamos el diccionario con dados y monedas: cara y cruz
 diccionario = {
     "dados" : {
 
@@ -14,23 +16,33 @@ diccionario = {
     }
 }
 
+#creamos un array con cara y cruz para la funcion tirar_moneda
 moneda = ["cara","cruz"]
+
+#función crear moneda
 def tirar_moneda():
     return random.choice(moneda)
 
+#creamos la misma función tirar_dado con valores del 1 al 6
 def tirar_dado():
     return random.randint(1,6)
 
+
 if __name__ == "__main__":
 
-
+#Creamos un bucle que recorra 10 veces para inicializar monedas a 0 en las 10 tiradas, se le suma uno porque si no sale nunca cara
+# por ejemlo hay que sumarle 0 veces también
     for x in range(11):
         diccionario["monedas"]["cara"][x] = 0
         diccionario["monedas"]["cruz"][x] = 0
 
+#Igual con los dados del 3 al 19, porque hay que tirar 3 veces los dados, entonces mínimo saldrá un 3 y maximo un 18
     for x in range(3,19):
         diccionario["dados"][x] = 0
 
+
+#Tiramos 1.000.000, 10 veces la moneda y sumamos al contador el número de veces que sale cara y otro con cruz para sumarle luego 1
+#ala posición del número de veces
     for x in range (1000000):
         contdados = 0
         contcara = 0
@@ -46,6 +58,7 @@ if __name__ == "__main__":
         diccionario["monedas"]["cruz"][contcruz] += 1
 
 
+#Mismo bucle con dados pero con tiradas de 3 en vez de 10
         for x in range(3):
             contdados += tirar_dado()
 
@@ -53,18 +66,73 @@ if __name__ == "__main__":
         diccionario["dados"][contdados] +=1
 
 
+#Imprimimos el diccionario
     pprint(diccionario)
-    nvecesmoneda = {5000,10000,20000,50000,100000,250000,500000,1000000}
 
+    #Obtengo un diccionario más manejable solo con
+    # los datos que me interesan(las veces que ha salido cara)
+    dCaras = diccionario["monedas"]["cara"]
+    pprint(dCaras)
 
-    fig, ax = plt.subplots()
-    # Colocamos una etiqueta en el eje Y
-    ax.set_ylabel('Nº caras')
-    # Colocamos una etiqueta en el eje X
-    ax.set_title('Caras')
-    # Creamos la grafica de barras utilizando 'paises' como eje X y 'ventas' como eje y.
-    plt.bar(diccionario["monedas"]["cara"],nvecesmoneda)
-    plt.savefig('barras_simple.png')
-    # Finalmente mostramos la grafica con el metodo show()
+    #En los diccionarios, no podemos suponer en las claves
+    #están ordenadas, por lo que, para la lista de valores
+    #del eje x, ordeno las claves
+    dcx = sorted(dCaras.keys())
+    print(dcx)
+
+    #Creo la lista de valores x en el mismo orden que tiene las x
+    #para que este ordenado
+    dcy = []
+    for x in dcx:
+        dcy.append(dCaras[x])
+    pprint(dcy)
+
+    #Utilizo matplotlib para imprimir
+    plt.figure()
+    plt.plot(dcx,dcy)
+    plt.xlabel("Nº caras")
+    plt.ylabel("Nº veces")
     plt.show()
+
+
+
+    # Hacemos la misma gráfica con las cruces
+    dCruz = diccionario["monedas"]["cruz"]
+    pprint(dCruz)
+
+    dcx = sorted(dCruz.keys())
+    print(dcx)
+
+    dcy = []
+    for x in dcx:
+        dcy.append(dCruz[x])
+    pprint(dcy)
+
+    plt.figure()
+    plt.plot(dcx, dcy)
+    plt.xlabel("Nº cruces")
+    plt.ylabel("Nº veces")
+    plt.show()
+
+
+    # Igual con los dados
+    dDados = diccionario["dados"]
+    pprint(dDados)
+
+    dcx = sorted(dDados.keys())
+    print(dcx)
+
+    dcy = []
+    for x in dcx:
+        dcy.append(dDados[x])
+    pprint(dcy)
+
+    plt.figure()
+    plt.plot(dcx, dcy)
+    plt.xlabel("Nº dados")
+    plt.ylabel("Nº veces")
+    plt.show()
+
+
+
 
